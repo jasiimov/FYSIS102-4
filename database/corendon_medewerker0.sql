b650c2a068cff4757305235fda34e25d83c81c4a
jasiimov <jasperbaars@hotmail.nl>
2016-12-22 13:51:47 +0100

    test
    ses

---
+++ database/corendon_medewerker0.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: corendon
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `medewerker`
+--
+
+DROP TABLE IF EXISTS `medewerker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medewerker` (
+  `idmedewerker` int(11) NOT NULL,
+  `voornaam` varchar(45) DEFAULT NULL,
+  `achternaam` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `tussenvoegsel` varchar(45) DEFAULT NULL,
+  `geslacht` varchar(45) DEFAULT NULL,
+  `woonplaats` varchar(45) DEFAULT NULL,
+  `adres` varchar(45) DEFAULT NULL,
+  `postcode` varchar(45) DEFAULT NULL,
+  `telefoonnummer` varchar(45) DEFAULT NULL,
+  `admin` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`idmedewerker`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medewerker`
+--
+
+LOCK TABLES `medewerker` WRITE;
+/*!40000 ALTER TABLE `medewerker` DISABLE KEYS */;
+INSERT INTO `medewerker` VALUES (1,'ad','min','admin@test.nl',NULL,'M','M',NULL,NULL,NULL,0);
+/*!40000 ALTER TABLE `medewerker` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-12-09 17:06:14
